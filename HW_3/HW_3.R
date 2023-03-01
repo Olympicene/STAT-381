fa78a7433ee85c01495788fd1759acf367ec61e7
Olympicene <kevinjcord@gmail.com>
2023-03-01 12:23:57 -0600

    added start of HW4 and finished HW3

---
+++ HW_3/HW_3.R
@@ -0,0 +1,8 @@
+pnorm(101, mean = 108, sd = sqrt(49), lower.tail = TRUE)
+
+qnorm(0.84, mean = 86, sd = sqrt(36), lower.tail = FALSE)
+
+pexp(0.4, rate = 1/5, lower.tail = FALSE)
+
+qnorm(0.057, lower.tail = FALSE)
+
